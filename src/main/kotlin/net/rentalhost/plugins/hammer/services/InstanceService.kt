ff107071d54e74bd77b6a71a66e7ff3f8bc29627
David Rodrigues <david.rodrigues@rentalhost.net>
2022-08-17 18:24:05 -0300

    [Changed] ClassService: use direct PhpIndex for class search;

--- src/main/kotlin/net/rentalhost/plugins/hammer/services/InstanceService.kt
+++
@@ -1,7 +0,0 @@
-package net.rentalhost.plugins.hammer.services
-
-import com.jetbrains.php.lang.psi.stubs.indexes.PhpClassIndex
-
-object InstanceService {
-    val classesIndex: PhpClassIndex = PhpClassIndex()
-}
